37d73c1b2934f10784c312edfad57bbc57108dd5
Chris Williams <williamsc224@mymacewan.ca>
2017-11-20 10:35:57 -0700

    Graph divs (#38)
    * Graphs are now in their own divs
    
    * Graphs have borders and are sitting in individual divs with visible labels and titles
    
    * fix merge conflicts
    
    * remove css that wasn't doing anything

--- traffic_data/graph/static/graph/barGraph.js
+++ traffic_data/graph/static/graph/barGraph.js
@@ -1,30 +1,31 @@
-function makeGraph(data, title, xTitle, yTitle){
+function makeGraph(data, title, divName, xTitle, yTitle){
 
-    var svg = dimple.newSvg(".graphs", "100%", "100%");
+    var svg = dimple.newSvg(divName, "100%", "100%");
     var chart = new dimple.chart(svg, data);
     chart.addCategoryAxis("y", yTitle);
     chart.addMeasureAxis("x", xTitle);
     chart.addSeries(null, dimple.plot.bar);
+    chart.setMargins("20%", "20%", "10%", "15%");
 
     svg.append("text")
     .attr("x", chart._xPixels() + chart._widthPixels() / 2)
-    .attr("y", chart._yPixels() - 20)
+    .attr("y", chart._yPixels() - 40)
     .attr("text-anchor", "middle")
     .style("font-size", "16px")
     .style("text-decoration", "underline")
     .text(title);
-
+    
     return chart;
 }
 
-function makeSocialGraph(data, title, xTitle){
-   chart = makeGraph(data, title, xTitle, "User");
+function makeSocialGraph(data, divName, title, xTitle){
+   chart = makeGraph(data, title, divName, xTitle, "User");
    chart.axes[0].addOrderRule(xTitle, false);
    chart.draw();
 }
 
-function makeSentimentGraph(data, title, xTitle){
-    chart = makeGraph(data, title, xTitle, "Sentiment");
+function makeSentimentGraph(data, divName, title, xTitle){
+    chart = makeGraph(data, title, divName, xTitle, "Sentiment");
     chart.axes[0].addOrderRule(["Positive", "Neutral", "Negative"]);
     chart.draw();   
 }
