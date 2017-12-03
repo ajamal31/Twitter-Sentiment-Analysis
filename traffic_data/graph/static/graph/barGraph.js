7295dc7a83bfa4cb6f2458db55253599a88fe44d
Chris Williams <williamsc224@mymacewan.ca>
2017-12-02 19:57:25 -0700

    Bargraph interaction (#74)
    * Non selected bars fade on mousover. Tooltip gone
    
    * original tooltips are back
    
    * tooltips are back and have larger text
    
    * change order of sentiment graph. Actually remove console.log. Add draw timer back.

--- traffic_data/graph/static/graph/barGraph.js
+++ traffic_data/graph/static/graph/barGraph.js
@@ -6,8 +6,16 @@ function makeGraph(data, title, divName, xTitle, yTitle) {
     var chart = new dimple.chart(svg, data);
     chart.addCategoryAxis("y", yTitle);
     chart.addMeasureAxis("x", xTitle);
-    chart.addSeries(null, dimple.plot.bar);
+    var s = chart.addSeries(null, dimple.plot.bar);
     chart.setMargins("20%", "20%", "10%", "15%");
+
+    s.tooltipFontSize = "14px";
+    s.getTooltipText = function (e) {
+        tooltip = [];
+        tooltip.push(yTitle + ": " + e.cy);
+        tooltip.push("Tweets" + ": " + e.cx);
+        return tooltip;
+    }
 
     svg.append("text")
         .attr("x", chart._xPixels() + chart._widthPixels() / 2)
@@ -17,21 +25,37 @@ function makeGraph(data, title, divName, xTitle, yTitle) {
         .style("text-decoration", "underline")
         .text(title);
 
-    return chart;
+    if (yTitle === "Sentiment") {
+        chart.axes[0].addOrderRule(["Negative", "Neutral","Positive"]);
+    }
+    else {
+        chart.axes[0].addOrderRule(xTitle, false);
+    }
+
+    chart.draw(draw_time);
+
+    s.shapes.on("mouseover", function (e) {
+        d3.select(divName).selectAll("rect").style("opacity", .3);
+        d3.select(this).style("opacity", 0.8);
+        dimple._showBarTooltip(e, this, chart, s);
+    });
+
+    s.shapes.on("mouseleave", function (e) {
+        d3.selectAll("rect").style("opacity", 0.8);
+        dimple._removeTooltip(e, this, chart, s);
+    })
 }
 
 function makeSocialGraph(data, divName, title, xTitle) {
-    chart = makeGraph(data, title, divName, xTitle, "User");
-    chart.axes[0].addOrderRule(xTitle, false);
-    chart.draw(draw_time);
-    return;
+    makeGraph(data, title, divName, xTitle, "User");
 }
 
 function makeSentimentGraph(data, divName, title, xTitle) {
+    makeGraph(data, title, divName, xTitle, "Sentiment");
+}
+   
+function makeSentimentGraph(data, divName, title, xTitle) {
     chart = makeGraph(data, title, divName, xTitle, "Sentiment");
-    chart.axes[0].addOrderRule(["Positive", "Neutral", "Negative"]);
-    chart.draw(draw_time);
-    return;
 }
 
 function redraw_socialgraph(data, object_name, title, x_title) {
